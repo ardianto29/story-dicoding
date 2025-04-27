d1528fa7d8214a383fd05adfc5d3ea50318517bf
Ardianto29 <ardiantoramadhan83@gmail.com>
2025-04-27 23:48:27 +0700

    Refactor routes.js for consistent import formatting and improved readability

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,19 +1,18 @@
-// src/scripts/routes/routes.js
-
-import HomePage      from '../pages/home-page.js';
-import AboutPage     from '../pages/about-page.js';
-import LoginPage     from '../pages/login-page.js';
-import RegisterPage  from '../pages/register-page.js';
-import AddStoryPage  from '../pages/add-story-page.js';
-import DetailPage    from '../pages/detail-page.js';
+import HomePage from "../pages/home-page.js";
+import AboutPage from "../pages/about-page.js";
+import LoginPage from "../pages/login-page.js";
+import RegisterPage from "../pages/register-page.js";
+import AddStoryPage from "../pages/add-story-page.js";
+import DetailPage from "../pages/detail-page.js";
 
 const routes = {
-  '/':            new HomePage(),
-  '/about':       new AboutPage(),
-  '/login':       new LoginPage(),
-  '/register':    new RegisterPage(),
-  '/add-story':   new AddStoryPage(),
-  '/detail/:id':  new DetailPage(),
+  "/": new HomePage(),
+  "/about": new AboutPage(),
+  "/login": new LoginPage(),
+  "/register": new RegisterPage(),
+  "/add-story": new AddStoryPage(),
+  "/stories/:id": new DetailPage(),
+  "/detail/:id": new DetailPage()
 };
 
 export default routes;
