395b77eb360f8cc8427d93cd8b19a8c09ca7b07f
Ardianto29 <ardiantoramadhan83@gmail.com>
2025-04-27 23:47:52 +0700

    Refactor AboutPage class to improve code formatting and remove unnecessary comments

--- src/scripts/pages/about-page.js
+++ src/scripts/pages/about-page.js
@@ -1,14 +1,12 @@
 export default class AboutPage {
-    async render() {
-      return `
+  async render() {
+    return `
         <section class="container">
           <h1>About Page</h1>
         </section>
       `;
-    }
-  
-    async afterRender() {
-      // Do your job here
-    }
   }
+
+  async afterRender() {}
+}
   
