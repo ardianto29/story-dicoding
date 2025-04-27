0ebd1b7de731eb22415ab14b0861edc6b7a985e8
Ardianto29 <ardiantoramadhan83@gmail.com>
2025-04-27 23:48:07 +0700

    Reformat RegisterPage class for improved readability and consistency

--- src/scripts/pages/register-page.js
+++ src/scripts/pages/register-page.js
@@ -2,16 +2,15 @@ import RegisterView      from '../views/register-view.js';
 import RegisterPresenter from '../presenters/register-presenter.js';
 
 export default class RegisterPage {
-    // Hanya kembalikan template HTML
-    async render() {
-      const view = new RegisterView();
-      return view.getTemplate();
-    }
-  
-    // Setelah HTML terpasang, panggil presenter untuk bind event
-    async afterRender() {
-      const view = new RegisterView();
-      const presenter = new RegisterPresenter(view);
-      presenter.init();
-    }
+  // Hanya kembalikan template HTML
+  async render() {
+    const view = new RegisterView();
+    return view.getTemplate();
   }
+
+  async afterRender() {
+    const view = new RegisterView();
+    const presenter = new RegisterPresenter(view);
+    presenter.init();
+  }
+}
