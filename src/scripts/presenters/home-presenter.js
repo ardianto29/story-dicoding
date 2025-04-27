0f06cbf00010f94a630a8d5f8d505687c05abc30
Ardianto29 <ardiantoramadhan83@gmail.com>
2025-04-27 23:48:18 +0700

    Refactor home-presenter.js for consistent import formatting and remove unnecessary comment

--- src/scripts/presenters/home-presenter.js
+++ src/scripts/presenters/home-presenter.js
@@ -1,6 +1,5 @@
-// src/scripts/presenters/home-presenter.js
-import { getAllStories } from '../data/api.js';
-import { getToken } from '../utils/index.js';
+import { getAllStories } from "../data/api.js";
+import { getToken } from "../utils/index.js";
 
 export default class HomePresenter {
   constructor(view) {
@@ -9,10 +8,9 @@ export default class HomePresenter {
 
   async init() {
     const token = getToken();
-    // location=1 agar kita hanya ambil stories yg punya lat/lon
     const { error, listStory } = await getAllStories(token, 1, 10, 1);
     if (error) {
-      console.error('Gagal fetch story:', listStory);
+      console.error("Gagal fetch story:", listStory);
       return;
     }
     this.view.showStories(listStory);
