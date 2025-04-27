f39b6a6ed165878af326c1da227a6068d54df193
Ardianto29 <ardiantoramadhan83@gmail.com>
2025-04-27 23:48:03 +0700

    Remove unnecessary comments from login-page.js for cleaner code

--- src/scripts/pages/login-page.js
+++ src/scripts/pages/login-page.js
@@ -2,13 +2,11 @@ import LoginView      from '../views/login-view.js';
 import LoginPresenter from '../presenters/login-presenter.js';
 
 export default class LoginPage {
-  // Hanya kembalikan template HTML
   async render() {
     const view = new LoginView();
     return view.getTemplate();
   }
 
-  // Setelah HTML terpasang, pasang presenter
   async afterRender() {
     const view = new LoginView();
     const presenter = new LoginPresenter(view);
