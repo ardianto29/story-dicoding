52fd2eb955f79eec092599301f7ab40602c06182
Ardianto29 <ardiantoramadhan83@gmail.com>
2025-05-15 11:27:36 +0700

    Refactor FavoritesPresenter to use class syntax and improve initialization

--- src/scripts/presenters/favorites-presenter.js
+++ src/scripts/presenters/favorites-presenter.js
@@ -1,36 +1,19 @@
 // src/scripts/presenters/favorites-presenter.js
-
 import FavoritesView from "../views/favorites-view.js";
 import { getAllFavorites, removeFavorite } from "../utils/db.js";
 
-const FavoritesPresenter = {
-  async init() {
-    // 1. Prepare view
-    const view = new FavoritesView();
-    view.mount("#app");
-
-    // 2. Bind handler hapus favorit ke Presenter
-    view.onRemove = async (id) => {
-      try {
-        await removeFavorite(id);
-        alert("Favorit dihapus");
-        const updated = await getAllFavorites();
-        view.render(updated);
-      } catch (err) {
-        console.error("Gagal menghapus favorit:", err);
-        alert("Gagal menghapus favorit");
-      }
-    };
-
-    // 3. Fetch data dan render list awal
-    try {
-      const items = await getAllFavorites();
-      view.render(items);
-    } catch (err) {
-      console.error("Gagal memuat favorit:", err);
-      alert("Gagal memuat favorit");
-    }
+export default class FavoritesPresenter {
+  constructor(containerSelector) {
+    this.view = new FavoritesView();
+    this.view.mount(containerSelector);
   }
-};
 
-export default FavoritesPresenter;
+  async init() {
+    const data = await getAllFavorites();
+    this.view.renderFavorites(data, {
+      onRemove: (id) => {
+        removeFavorite(id).then(() => this.init());
+      }
+    });
+  }
+}
